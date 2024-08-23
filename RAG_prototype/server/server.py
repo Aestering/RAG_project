716b5768eb4b946160c3102e689dceee824aaaa4
Aestering <157222872+Aestering@users.noreply.github.com>
2024-08-23 11:58:08 +0800

    Add files via upload

--- RAG_prototype/server/server.py
+++ RAG_prototype/server/server.py
@@ -9,28 +9,38 @@ os.environ["YI_API_KEY"] = "dc2935e111024862885468d5cbe358af"
 app = Flask(__name__)
 CORS(app)
 
+
+
 global index
 
-def create_llama_index():
+def load_index():
+    from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+    documents = SimpleDirectoryReader("uploads").load_data()
+    index = VectorStoreIndex.from_documents(documents)
+    index.storage_context.persist(persist_dir="index")
+
+def load_or_create_llama_index():
+    global index
     try:
         index_dir = "index"
         os.makedirs(index_dir, exist_ok=True)
 
         from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
+        from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 
-        from llama_index.embeddings.huggingface import HuggingFaceEmbedding 
         Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
 
-        documents = SimpleDirectoryReader("uploads").load_data()
-        index = VectorStoreIndex.from_documents(documents)
-        index.storage_context.persist(persist_dir=index_dir)
+        if os.path.exists(index_dir) and os.listdir(index_dir):
+            from llama_index.core import StorageContext, load_index_from_storage
+            storage_context = StorageContext.from_defaults(persist_dir=index_dir)
+            index = load_index_from_storage(storage_context)
+            print("Index loaded successfully.")
+        else:
+            load_index()
+            print("New index created and persisted.")
 
-        if not os.path.exists(index_dir) or not os.listdir(index_dir):
-            return "Error: in indexing document", 400
-        
-        return jsonify({"message": "File indexed successfully"})
     except Exception as e:
-        return f"An error occurred: {e}", 400
+        print(f"An error occurred during index creation/loading: {e}")
 
 def query_index():
     # retrieve the api key
@@ -87,12 +97,14 @@ def upload_file():
         return jsonify({'error': 'No selected file'})
 
     if file:
-        upload_dir = 'uploads'  # Specify the directory where you want to save uploaded files
+        upload_dir = 'uploads'
         os.makedirs(upload_dir, exist_ok=True)
 
         file.save(os.path.join(upload_dir, file.filename))
 
-        return create_llama_index()
+        load_index()
+        load_or_create_llama_index()
+        return jsonify({"message": "File uploaded and indexed successfully"})
 
 @app.route('/translate', methods=['POST'])
 def translate():
@@ -112,4 +124,5 @@ def translate():
 
 
 if __name__ == "__main__":
+    load_or_create_llama_index()
     app.run()
