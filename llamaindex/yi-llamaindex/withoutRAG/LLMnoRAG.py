d38271150f73bbe2e53391210d34493f6fb255df
Aestering <157222872+Aestering@users.noreply.github.com>
2026-03-29 20:09:50 +0800

    Update LLMnoRAG.py

--- llamaindex/yi-llamaindex/withoutRAG/LLMnoRAG.py
+++ llamaindex/yi-llamaindex/withoutRAG/LLMnoRAG.py
@@ -1,7 +1,7 @@
 from llama_index.llms.openai_like import OpenAILike
 from llama_index.core.llms import ChatMessage
 
-model = OpenAILike(api_base="https://api.01.ai/v1", api_key = "dc2935e111024862885468d5cbe358af", model="yi-large", is_chat_model=True)
+model = OpenAILike(api_base="https://api.01.ai/v1", api_key = "###", model="yi-large", is_chat_model=True)
 
 response = model.chat(messages=[ChatMessage(content="Hi, Who are you?")])
 print(response)
