2eda0ff7c24e67f47a66c4e2fc029d74db2c0e81
Aestering <157222872+Aestering@users.noreply.github.com>
2026-03-29 20:04:35 +0800

    Update mistral.py

--- haystack/mistral-haystack/norag/mistral.py
+++ haystack/mistral-haystack/norag/mistral.py
@@ -2,7 +2,7 @@ import os
 from haystack.dataclasses import ChatMessage
 from haystack_integrations.components.generators.mistral import MistralChatGenerator
 
-os.environ["MISTRAL_API_KEY"] = "3GyMujRYXjA3Ti37gq0vCEjQVYTu9ATk"
+os.environ["MISTRAL_API_KEY"] = "###"
 model = "mistral-medium"
 
 client = MistralChatGenerator(model=model)
