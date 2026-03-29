2f6c247f6854916263b1b0dd893949716df7cd4f
Aestering <157222872+Aestering@users.noreply.github.com>
2026-03-29 20:05:51 +0800

    Update mistral.py

--- langchain-test/mistral-langchain/noRAG/mistral.py
+++ langchain-test/mistral-langchain/noRAG/mistral.py
@@ -1,6 +1,6 @@
 from langchain_core.messages import HumanMessage
 from langchain_mistralai.chat_models import ChatMistralAI
-api_key="3GyMujRYXjA3Ti37gq0vCEjQVYTu9ATk"
+api_key="###"
 
 chat = ChatMistralAI(api_key=api_key)
 
