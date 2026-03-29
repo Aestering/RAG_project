a16fca2642db065b8bdce6e2a699ccc3bfb53380
Aestering <157222872+Aestering@users.noreply.github.com>
2026-03-29 20:07:41 +0800

    Update mistralChat.py

--- llamaindex/mistral-llamaindex/noRAG/mistralChat.py
+++ llamaindex/mistral-llamaindex/noRAG/mistralChat.py
@@ -7,5 +7,5 @@ messages = [
     ChatMessage(role = "system", content = "You are CEO of MistralAI."),
     ChatMessage(role = "user", content = "Tell me the story about La plateforme"),
 ]
-resp = MistralAI(api_key="3GyMujRYXjA3Ti37gq0vCEjQVYTu9ATk").chat(messages)
+resp = MistralAI(api_key="###").chat(messages)
 print(resp)
