80844ba92d7e4eff5734d2e38824d3e55d836af8
Aestering <157222872+Aestering@users.noreply.github.com>
2026-03-29 20:07:05 +0800

    Update llama.py

--- llamaindex/llama2-llamaindex/norag/llama.py
+++ llamaindex/llama2-llamaindex/norag/llama.py
@@ -1,6 +1,6 @@
 from llama_index.llms.llama_api import LlamaAPI
 
-api_key = "LL-WdNfTVjFcJO83a6K5gdLGTKsUpUZi3chAoZcarQILe60gj1EiuHRU7oRkynIR227"
+api_key = "###"
 
 llm = LlamaAPI(api_key=api_key)
 
